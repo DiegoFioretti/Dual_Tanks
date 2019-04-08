e5b5d0e51565fdbc7b7fcb062bd4b8f854299144
Nico <diego_fioretti_97@hotmail.com>
2019-04-08 13:10:18 -0300

    First Commit
    - Project Initialized
    - First iteration of arched shot

---
+++ Assets/CalculadorOblicuo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculadorOblicuo : MonoBehaviour
+{
+    [SerializeField] private float Angulodedisparo;
+    [SerializeField] private float Velocidadinicial;
+    [SerializeField] private float Gravedad;
+
+    private float VelocidadX;
+    private float VelocidadY;
+    private float TimeY;
+
+    void Start(){
+        VelocidadX = Velocidadinicial * Mathf.Cos(Angulodedisparo);
+        TimeY = (Velocidadinicial * Mathf.Sin(Angulodedisparo)) / Gravedad;
+        VelocidadY = Velocidadinicial * Mathf.Sin(Angulodedisparo) - Gravedad * TimeY;
+    }
+
+    void Update(){
+        transform.Translate(VelocidadX,VelocidadY,0);
+        VelocidadY -= Gravedad;
+    }
+}
