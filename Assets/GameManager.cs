0ce7b07d06a21b19e0a43b678e230b7469616002
Nico <diego_fioretti_97@hotmail.com>
2019-04-08 23:24:25 -0300

    Movement Implementation
    - Created a GameManager to manage the turns
    - Created script that allows horizontal movement for players

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float SwitchTime;
+
+    private bool Player1Turn;
+
+    void Start(){
+        Player1Turn = true;
+
+        InvokeRepeating("SwitchPlayerTurn", SwitchTime, SwitchTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchPlayerTurn() {
+        if (Player1Turn == true)
+            Player1Turn = false;
+        else
+            Player1Turn = true;
+    }
+
+    public bool GetIfPlayer1Turn{
+        get { return Player1Turn; }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private bool IsPlayer1;
+    [SerializeField] private float Speed;
+
+    Camera camara;
+    Vector3 camarapos;
+    float camheight;
+    float camwidth;
+    float camleft;
+    float camright;
+
+    GameManager thegamemanager;
+
+    void Start(){
+        thegamemanager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        camara = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        camarapos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+        camheight = 2f * camara.orthographicSize;
+        camwidth = camheight * camara.aspect;
+        camright = camarapos.x + (camwidth / 2);
+        camleft = camarapos.x - (camwidth / 2);
+    }
+
+    void Update(){
+        if (thegamemanager.GetIfPlayer1Turn == IsPlayer1){
+            if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < camright && !Input.GetKey(KeyCode.LeftArrow))
+                transform.Translate(Speed * Time.deltaTime, 0 , 0);
+            if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > camleft && !Input.GetKey(KeyCode.RightArrow))
+                transform.Translate(-Speed * Time.deltaTime, 0 , 0);
+        }
+    }
+}
