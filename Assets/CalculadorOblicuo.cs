0e89f7ef203d44abf424faa247c086905982d7cc
sfsd <diego_fioretti_97@hotmail.com>
2019-04-10 20:21:42 -0300

    Added functions to shoot bullets
    -Now there are proper getters and setters for the bullet's speed and angle
    -Using the up/down arrows keys and holding the shoot button allow to modify both the bullet's angle and speed respectively

--- Assets/CalculadorOblicuo.cs
+++ Assets/CalculadorOblicuo.cs
@@ -10,16 +10,42 @@ public class CalculadorOblicuo : MonoBehaviour
 
     private float VelocidadX;
     private float VelocidadY;
-    private float TimeY;
+
+    // Observation 3.14 = 180º, so logically 1.57 is 90º, also 0 shoots to the RIGHT so 3.14 shoots LEFT
+    // This is because it calculates the angles in RADIANS instead of DEGREES
+    // So when showing the degrees in the UI use Mathf.Rad2Deg to show the correct value
+    // degree = radian * Mathf.Rad2Deg ---> show "degree"
 
     void Start(){
         VelocidadX = Velocidadinicial * Mathf.Cos(Angulodedisparo);
-        TimeY = (Velocidadinicial * Mathf.Sin(Angulodedisparo)) / Gravedad;
-        VelocidadY = Velocidadinicial * Mathf.Sin(Angulodedisparo) - Gravedad * TimeY;
+        VelocidadY = Velocidadinicial * Mathf.Sin(Angulodedisparo) - Gravedad;
+        InvokeRepeating("ResetPosition", 1, 2);
     }
 
     void Update(){
         transform.Translate(VelocidadX,VelocidadY,0);
         VelocidadY -= Gravedad;
+    }
+
+    public float ConfigurarVelocidad{
+        get { return Velocidadinicial; }
+        set {
+            if (value < 0)
+                Velocidadinicial = 0.0f;
+            else
+                Velocidadinicial = value;
+        }
+    }
+
+    public float ConfigurarAngulo{
+        get { return Angulodedisparo; }
+        set{
+            if (value < 0)
+                Angulodedisparo = 0.0f;
+            else if (value > 3.14)
+                Angulodedisparo = 3.14f;
+            else
+                Angulodedisparo = value;
+        }
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool IsPlayer1;
     [SerializeField] private float Speed;
     [SerializeField] private GameObject Bullet;
+    [SerializeField] private float AngleModifyRate;
+    [SerializeField] private float SpeedModifyRate;
+    [SerializeField] private float MaxBulletSpeed;
 
     Camera camara;
     Vector3 camarapos;
@@ -18,16 +21,20 @@ public class PlayerController : MonoBehaviour
     float shootSpeed;
     float shootAngle;
 
+    bool speedIncreasing;
+
     GameManager thegamemanager;
+    CalculadorOblicuo calculadorBullet;
 
     void Start(){
-        thegamemanager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        thegamemanager = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameManager>();
         camara = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         camarapos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
         camheight = 2f * camara.orthographicSize;
         camwidth = camheight * camara.aspect;
         camright = camarapos.x + (camwidth / 2);
         camleft = camarapos.x - (camwidth / 2);
+        calculadorBullet = Bullet.GetComponent<CalculadorOblicuo>();
     }
 
     void Update(){
@@ -37,17 +44,34 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > camleft && !Input.GetKey(KeyCode.RightArrow))
                 transform.Translate(-Speed * Time.deltaTime, 0 , 0);
             if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow)){
-                //Increase shootAngle
+                //With 0.01 gravity its good to increment at a 0.01 rate
+                shootAngle += AngleModifyRate;
+                if (shootAngle >= 0)
+                    shootAngle = 0;
+                calculadorBullet.ConfigurarAngulo = shootAngle;
             }
             if (Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.UpArrow)){
-                //Increase shootAngle
+                //With 0.01 gravity its good to decrease at a 0.01 rate
+                shootAngle -= AngleModifyRate;
+                if (shootAngle >= 0)
+                    shootAngle = 0;
+                calculadorBullet.ConfigurarAngulo = shootAngle;
             }
             if (Input.GetKey(KeyCode.Space)){
-                //Increase shootSpeed
+                //With 0.01 gravity its good to set a Max of 0.5
+                if (speedIncreasing)
+                    shootSpeed += SpeedModifyRate;
+                if (!speedIncreasing)
+                    shootSpeed -= SpeedModifyRate;
+                if (shootSpeed <= 0)
+                    speedIncreasing = true;
+                if (shootSpeed >= MaxBulletSpeed)
+                    speedIncreasing = false;
+                calculadorBullet.ConfigurarAngulo = shootSpeed;
             }
             if (Input.GetKeyUp(KeyCode.Space)){
                 //Shoot bullet
-                Instantiate(Bullet, transform);
+                Bullet.transform.position = transform.position;
             }
         }
     }
